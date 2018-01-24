8f1456940506037aecaf37a366f5a9ba302125d2
luozhouyang <stupidme.me.lzy@gmail.com>
2018-01-24 21:11:22 +0800

    Create infer data set

--- nmt/dataset.py
+++ nmt/dataset.py
@@ -1,11 +1,21 @@
-import tensorflow as tf
-import tensorflow.contrib.keras.api.keras.backend as K
+import collections
 
-from .utils import vocab_utils
+import tensorflow as tf
+
+
+class BatchedInput(
+    collections.namedtuple(
+        "BatchedInput",
+        ("initializer",
+         "source",
+         "target_input",
+         "target_output",
+         "source_sequence_length",
+         "target_sequence_length"))):
+    pass
 
 
 class Dataset(object):
-
     def src_vocab_table(self):
         raise NotImplementedError("Not implemented yet.")
 
@@ -23,7 +33,6 @@ class Dataset(object):
 
 
 class TrainDataset(Dataset):
-
     def src_vocab_table(self):
         pass
 
@@ -41,27 +50,36 @@ class TrainDataset(Dataset):
 
 
 class InferDataset(Dataset):
-
     def __init__(self,
-                 src_vocab_file,
-                 tgt_vocab_file,
-                 share_vocab,
-                 src_file,
-                 tgt_file,
+                 src_dataset,
+                 src_vocab_table,
                  batch_size,
                  eos,
                  src_max_len=None):
-        self.src_vocab_table, self.tgt_vocab_table = vocab_utils.create_vocab_table(
-            src_vocab_file, tgt_vocab_file, share_vocab)
+        self.src_vocab_table = src_vocab_table
         self.src_eos_id = tf.cast(self.src_vocab_table.lookup(tf.constant(eos)), tf.int32)
 
-        self.src_placeholder = K.placeholder(shape=[None], dtype=tf.string)
-        self.batch_size_placeholder = K.placeholder(shape=[], dtype=tf.int64)
-        src_dataset = tf.data.Dataset.from_tensor_slices(self.src_placeholder)
         src_dataset = src_dataset.map(lambda src: tf.string_split([src]).values)
         if src_max_len:
             src_dataset = src_dataset.map(lambda src: src[:src_max_len])
         src_dataset = src_dataset.map(lambda src: tf.cast(self.src_vocab_table.lookup(src), tf.int32))
+        src_dataset = src_dataset.map(lambda src: (src, tf.size(src)))
+
+        def batching_func(ds):
+            return ds.padded_batch(
+                batch_size=batch_size,
+                padded_shapes=(
+                    tf.TensorShape([None]),
+                    tf.TensorShape([])
+                ),
+                padding_values=(
+                    self.src_eos_id,
+                    0
+                )
+            )
+
+        self.batched_dataset = batching_func(src_dataset)
+        self.batched_iterator = self.batched_dataset.make_initializable_iterator()
 
     def src_vocab_table(self):
         return self.src_vocab_table
@@ -70,10 +88,18 @@ class InferDataset(Dataset):
         return self.tgt_vocab_table
 
     def batched_dataset(self):
-        pass
+        return self.batched_dataset
 
     def batched_iterator(self):
-        pass
+        return self.batched_iterator
 
     def batched_input(self):
-        pass
+        src_ids, src_seq_len = self.batched_iterator.get_next()
+        return BatchedInput(
+            initializer=self.batched_iterator.initializer,
+            source=src_ids,
+            target_input=None,
+            target_output=None,
+            source_sequence_length=src_seq_len,
+            target_sequence_length=None
+        )
