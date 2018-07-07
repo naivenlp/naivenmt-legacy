2e6b69579d6f2d61bf4fbf276469fb59003af54a
luozhouyang <stupidme.me.lzy@gmail.com>
2018-07-07 17:40:14 +0800

    Add serving input fn

--- nmt/estimator/inputter.py
+++ nmt/estimator/inputter.py
@@ -1,24 +1,36 @@
 import abc
+
 import tensorflow as tf
 
 
 class Inputter(abc.ABC):
 
-    @abc.abstractmethod
-    def build_dataset(self):
-        raise NotImplementedError()
+  def __init__(self, dtype=None):
+    self.dtype = dtype
+
+  @abc.abstractmethod
+  def build_dataset(self):
+    raise NotImplementedError()
+
+  @abc.abstractmethod
+  def get_serving_input_receiver(self):
+    raise NotImplementedError()
 
 
 class TextFileInputter(Inputter):
 
-    def __init__(self, file):
-        self.file = file
+  def __init__(self, file, dtype=None):
+    super().__init__(dtype)
+    self.file = file
 
-    def build_dataset(self):
-        return tf.data.TextLineDataset(self.file)
+  def build_dataset(self):
+    return tf.data.TextLineDataset(self.file)
+
+  def get_serving_input_receiver(self):
+    return None
 
 
 class WordEmbedder(TextFileInputter):
 
-    def build_dataset(self):
-        pass
+  def build_dataset(self):
+    pass
