e818722201937cb646af393bc1e4fb1993950ad6
luozhouyang <stupidme.me.lzy@gmail.com>
2018-07-07 17:39:54 +0800

    Create base model based on estimator

---
+++ nmt/estimator/utils.py
@@ -0,0 +1,10 @@
+def add_dict_to_collections(name, _dict):
+  raise NotImplementedError()
+
+
+def build_training_dataset():
+  raise NotImplementedError()
+
+
+def build_infer_dataset():
+  raise NotImplementedError()
