1c68ab84173ce238b32cdfeab530da89967b2e75
luozhouyang <stupidme.me.lzy@gmail.com>
2018-07-24 22:40:28 +0800

    Update signature of method

--- naivenmt/decoders.py
+++ naivenmt/decoders.py
@@ -1,8 +1,52 @@
 import abc
+import tensorflow as tf
+from tensorflow.python.layers import core
 
 
 class Decoder(abc.ABC):
 
   @abc.abstractmethod
-  def decode(self, encoder_outputs, encoder_state, labels, params):
+  def decode(self,
+             mode,
+             encoder_outputs,
+             encoder_state,
+             embedding_input,
+             sequence_length,
+             params):
     raise NotImplementedError()
+
+
+class DefaultDecoder(Decoder):
+
+  def __init__(self, scope="decoder"):
+    self.scope = scope
+
+  def decode(self,
+             mode,
+             encoder_outputs,
+             encoder_state,
+             embedding_input,
+             sequence_length,
+             params):
+    with tf.variable_scope(self.scope) as scope:
+      cell, decoder_initial_state = self._build_decoder_cell(
+        params, encoder_outputs, encoder_state, sequence_length)
+      if mode != tf.estimator.ModeKeys.PREDICT:
+        helper = tf.contrib.seq2seq.TrainingHelper(
+          embedding_input, sequence_length, time_major=params.time_major)
+        decoder = tf.contrib.seq2seq.BasicDecoder(
+          cell, helper, decoder_initial_state)
+        outputs, final_context_state, _ = tf.contrib.seq2seq.dynamic_decode(
+          decoder,
+          output_time_major=params.time_major,
+          swap_memory=True,
+          scope=scope)
+        sample_id = outputs.sample_id
+        # TODO(luozhouyang) Add `target_vocab_size` to Hparams
+        output_layer = core.Dense(
+          params.target_vocab_size, use_bias=False, name="output_projection")
+        logits = output_layer(outputs.rnn_output)
+      else:
+        beam_width = params.beam_width
+        length_penalty_weight = params.length_penalty_weight
+
--- naivenmt/encoders.py
+++ naivenmt/encoders.py
@@ -1,8 +1,19 @@
 import abc
+import tensorflow as tf
 
 
 class Encoder(abc.ABC):
 
   @abc.abstractmethod
-  def encode(self, features, labels):
+  def encode(self, embedding_input, sequence_length, params):
     raise NotImplementedError()
+
+
+class DefaultEncoder(Encoder):
+
+  def __init__(self, scope="encoder"):
+    self.scope = scope
+
+  def encode(self, embedding_input, sequence_length, params):
+    with tf.variable_scope(self.scope) as scope:
+      pass
