a7b26679deb1ba52a79bd95f885d698d5f3f289c
luozhouyang <stupidme.me.lzy@gmail.com>
2018-07-25 16:45:25 +0800

    Remove packages

--- utils/vocab_utils.py
+++
@@ -1,9 +0,0 @@
-def parse_vocab(vocab_file):
-    vocabs = []
-    with open(vocab_file, mode='rt', encoding='utf8', buffering=8192) as f:
-        for line in f:
-            line = line.strip(' ').strip('\n')
-            if not line:
-                continue
-            vocabs.append(line)
-    return vocabs
