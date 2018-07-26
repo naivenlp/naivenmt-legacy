b39ed46bb6c6c0257a46f6dda26cadcac4476baa
luozhouyang <stupidme.me.lzy@gmail.com>
2018-07-26 15:05:46 +0800

    Create encoders module

--- naivenmt/encoders/abstract_encoder.py
+++ naivenmt/encoders/abstract_encoder.py
@@ -1,19 +1,24 @@
 import abc
+
 import tensorflow as tf
 
 
-class Encoder(abc.ABC):
+class EncoderInterface(abc.ABC):
 
   @abc.abstractmethod
   def encode(self, embedding_input, sequence_length, params, configs):
     raise NotImplementedError()
 
 
-class DefaultEncoder(Encoder):
+class AbstractEncoder(EncoderInterface):
 
-  def __init__(self, scope="encoder"):
+  def __init__(self, embedding, scope="encoder"):
+    self.embedding = embedding
     self.scope = scope
 
   def encode(self, embedding_input, sequence_length, params, configs):
+    num_layers = params.num_encoder_layers
+    num_residual_layers = params.num_encoder_residual_layers
+
     with tf.variable_scope(self.scope) as scope:
       pass
