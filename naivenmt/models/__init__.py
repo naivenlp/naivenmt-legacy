19a2b57e3146600625c44c3865dd1e4a640778c7
luozhouyang <stupidme.me.lzy@gmail.com>
2018-07-27 14:28:42 +0800

    Add models

---
+++ naivenmt/models/__init__.py
@@ -0,0 +1,6 @@
+from .attention_model import AttentionModel
+from .basic_model import BasicDecoder
+from .gnmt_model import GNMTModel
+from .sequence_to_sequence import SequenceToSequence
+
+__all__ = ["BasicDecoder", "AttentionModel", "SequenceToSequence", "GNMTModel"]
---
+++ naivenmt/models/basic_model.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+from naivenmt.decoders import BasicDecoder
+from naivenmt.embedding import Embedding
+from naivenmt.encoders import BasicEncoder
+from naivenmt.inputters import Inputter
+from naivenmt.models import SequenceToSequence
+
+
+class BasicModel(SequenceToSequence):
+
+  def __init__(self, configs):
+    inputter = Inputter(config=configs, dtype=tf.float32)
+    embedding = Embedding(src_vocab_size=100000,
+                          tgt_vocab_size=100000,
+                          share_vocab=False,
+                          src_embedding_size=256,
+                          tgt_embedding_size=256,
+                          src_vocab_file=configs.src_vocab_file,
+                          tgt_vocab_file=configs.tgt_vocab_file)
+    encoder = BasicEncoder(embedding=embedding)
+    decoder = BasicDecoder(embedding=embedding)
+    super().__init__(inputter, encoder, decoder)
