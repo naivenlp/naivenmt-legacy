7b5fc3e5d7f8af3ac53a486dde57dd875880fb99
luozhouyang <stupidme.me.lzy@gmail.com>
2018-07-27 10:53:27 +0800

    Update init file

---
+++ naivenmt/decoders/__init__.py
@@ -0,0 +1,6 @@
+from .abstract_decoder import AbstractDecoder
+from .basic_decoder import BasicDecoder
+from .attention_decoder import AttentionDecoder
+from .gnmt_decoder import GNMTDecoder
+
+__all__ = ["AbstractDecoder", "BasicDecoder", "AttentionDecoder", "GNMTDecoder"]
---
+++ naivenmt/encoders/__init__.py
@@ -0,0 +1,5 @@
+from .abstract_encoder import AbstractEncoder
+from .basic_encoder import BasicEncoder
+from .gnmt_encoder import GNMTEncoder
+
+__all__ = ["AbstractEncoder", "BasicEncoder", "GNMTEncoder"]
