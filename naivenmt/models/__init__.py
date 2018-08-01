8548b1bfe1707a3cafecc0def65a6ca4edd546a7
luozhouyang <stupidme.me.lzy@gmail.com>
2018-08-01 14:08:56 +0800

    Fixed import

--- naivenmt/models/__init__.py
+++ naivenmt/models/__init__.py
@@ -1,6 +1,6 @@
 from .attention_model import AttentionModel
-from .basic_model import BasicDecoder
+from .basic_model import BasicModel
 from .gnmt_model import GNMTModel
 from .sequence_to_sequence import SequenceToSequence
 
-__all__ = ["BasicDecoder", "AttentionModel", "SequenceToSequence", "GNMTModel"]
+__all__ = ["BasicModel", "AttentionModel", "SequenceToSequence", "GNMTModel"]
