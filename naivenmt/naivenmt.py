f9f04f7cc71c2b55a5267944e6f62acde8f53dc2
luozhouyang <stupidme.me.lzy@gmail.com>
2018-12-04 17:32:28 +0800

    Refactoring naivenmt

--- naivenmt/naivenmt.py
+++ naivenmt/naivenmt.py
@@ -13,232 +13,172 @@
 # limitations under the License.
 # ==============================================================================
 
-import abc
 import argparse
-import codecs
+import functools
+import json
 import os
-import sys
 
 import tensorflow as tf
-from tensorflow.python.estimator.util import fn_args
 
-from naivenmt.configs.arguments import add_arguments
-from naivenmt.configs.hparams import Hparams
-from naivenmt.hooks import CkptLoggingListener
-from naivenmt.hooks import LifecycleLoggingHook
+from naivenmt.configs import HParamsBuilder
+from naivenmt.hooks import CountParamsHook
+from naivenmt.hooks import InitHook
 from naivenmt.hooks import SaveEvaluationPredictionsHook
-from naivenmt.hooks import TrainTensorsSummaryHook
-from naivenmt.models import BasicModel, AttentionModel, GNMTModel
-from naivenmt.utils import average_ckpts
+from naivenmt.models import AttentionModel
+from naivenmt.models import BasicModel
+from naivenmt.models import GNMTModel
+from naivenmt.utils import text_utils
 
 
-class NaiveNMTInterface(abc.ABC):
+class NaiveNMT(object):
 
-  @abc.abstractmethod
-  def train(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def eval(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def predict(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def export(self):
-    raise NotImplementedError()
-
-
-class AbstractNaiveNMT(NaiveNMTInterface):
-
-  def __init__(self, hparams):
+  def __init__(self, hparams, model):
     self.hparams = hparams
-    self.model = self._create_model()
-    self.estimator = self._create_estimator()
+    self.model = model
 
-  @abc.abstractmethod
-  def _create_train_hooks(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def _create_eval_hooks(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def _create_predict_hooks(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def _create_run_config(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def _create_session_config(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def _create_model_lifecycle_hooks(self):
-    raise NotImplementedError()
-
-  @abc.abstractmethod
-  def _create_model_tensors_hooks(self):
-    raise NotImplementedError()
-
-  def _create_estimator(self):
-    return tf.estimator.Estimator(
-      model_fn=self.model.model_fn(),
-      model_dir=self.hparams.out_dir,
-      config=self._create_run_config(),
-      params=self.hparams)
-
-  def _create_model(self):
-    lifecycle_hooks = self._create_model_lifecycle_hooks()
-    tensors_hooks = self._create_model_tensors_hooks()
-    if not self.hparams.attention:
-      return BasicModel(params=self.hparams,
-                        lifecycle_hooks=lifecycle_hooks,
-                        tensors_hooks=tensors_hooks)
-    if self.hparams.attention_architecture == "standard":
-      return AttentionModel(params=self.hparams,
-                            lifecycle_hooks=lifecycle_hooks,
-                            tensors_hooks=tensors_hooks)
-    if self.hparams.attention_architecture in ["gnmt", "gnmt_v2"]:
-      return GNMTModel(params=self.hparams,
-                       lifecycle_hooks=lifecycle_hooks,
-                       tensors_hooks=tensors_hooks)
-    raise ValueError("Can not create model.")
-
-  def train(self):
-    self.estimator.train(
-      input_fn=self.model.input_fn(tf.estimator.ModeKeys.TRAIN),
-      hooks=self._create_train_hooks(),
-      max_steps=self.hparams.num_train_steps)
-    if self.hparams.avg_ckpts:
-      average_ckpts(self.hparams.out_dir, self.hparams.num_keep_ckpts,
-                    self._create_session_config())
-
-  def eval(self):
-    self.estimator.evaluate(
-      input_fn=self.model.input_fn(tf.estimator.ModeKeys.EVAL),
-      hooks=self._create_eval_hooks(),
-      checkpoint_path=None)
-
-  def predict(self):
-    # inference_input_file is processed in ``Inputter``.
-    # infer_input_file = self.hparams.inference_input_file
-    # if not infer_input_file:
-    #   raise ValueError("Inference input file must be provided.")
-    infer_output_file = self.hparams.inference_output_file
-    if not infer_output_file:
-      infer_output_file = os.path.join(self.hparams.out_dir, "infer_output.txt")
-    checkpoint_path = self.hparams.ckpt
-    if not checkpoint_path:
-      checkpoint_path = tf.train.latest_checkpoint(self.hparams.out_dir)
-    predictions = self.estimator.predict(
-      input_fn=self.model.input_fn(tf.estimator.ModeKeys.PREDICT),
-      checkpoint_path=checkpoint_path,
-      hooks=self._create_predict_hooks())
-
-    with codecs.getwriter("utf-8")(
-            tf.gfile.GFile(infer_output_file, mode="wb")) as fout:
-      for prediction in predictions:
-        fout.write((prediction + b'\n').decode("utf-8"))
-
-  def export(self):
-    # Use flags.ckpt to create ckpt for exporting
-    checkpoint_path = self.hparams.ckpt
-    if not checkpoint_path:
-      checkpoint_path = tf.train.latest_checkpoint(self.hparams.out_dir)
-
-    # TODO(luozhouyang) add an option to set export dir
-    export_dir = os.path.join(self.estimator.model_dir, "export")
-    if not os.path.isdir(export_dir):
-      os.makedirs(export_dir)
-
-    kwargs = {}
-    if "strip_default_attrs" in fn_args(self.estimator.export_savedmodel):
-      # Set strip_default_attrs to True for TensorFlow 1.6+ to stay consistent
-      # with the behavior of tf.estimator.Exporter.
-      kwargs["strip_default_attrs"] = True
-
-    self.estimator.export_savedmodel(
-      export_dir,
-      serving_input_receiver_fn=self.model.serving_input_fn(),
-      checkpoint_path=checkpoint_path,
-      **kwargs)
-
-
-class NaiveNMT(AbstractNaiveNMT):
-
-  def _create_run_config(self):
-    config = tf.estimator.RunConfig(
-      model_dir=self.hparams.out_dir,
-      session_config=self._create_session_config(),
-      tf_random_seed=self.hparams.random_seed,
-      save_summary_steps=100,
-      save_checkpoints_steps=5000,
-      save_checkpoints_secs=None,
-      keep_checkpoint_max=10,
-      log_step_count_steps=500)
-    return config
-
-  def _create_session_config(self):
-    return tf.ConfigProto(
+    sess_config = tf.ConfigProto(
       allow_soft_placement=True,
       log_device_placement=False,
-      gpu_options=tf.GPUOptions(allow_growth=False))
+      gpu_options=tf.GPUOptions(allow_growth=True))
 
-  def _create_model_lifecycle_hooks(self):
-    return [LifecycleLoggingHook()]
+    # TODO(luozhouyang) Add distribution strategy
+    run_config = tf.estimator.RunConfig(
+      model_dir=self.hparams.out_dir,
+      session_config=sess_config,
+      tf_random_seed=self.hparams.random_seed,
+      save_checkpoints_secs=None,
+      save_checkpoints_steps=self.hparams.save_ckpt_steps,
+      keep_checkpoint_max=self.hparams.keep_ckpt_max,
+      log_step_count_steps=self.hparams.log_step_count_steps,
+      train_distribute=None)
 
-  def _create_model_tensors_hooks(self):
-    return [TrainTensorsSummaryHook()]
+    self.estimator = tf.estimator.Estimator(
+      model_fn=self.model.model_fn,
+      model_dir=self.hparams.out_dir,
+      params=self.hparams,
+      config=run_config)
 
-  def _create_train_hooks(self):
-    step_count_hook = tf.train.StepCounterHook(
-      every_n_steps=100, output_dir=self.hparams.out_dir)
-    ckpt_saver_hook = tf.train.CheckpointSaverHook(
-      save_steps=self.hparams,
-      checkpoint_dir=self.hparams.out_dir,
-      listeners=[CkptLoggingListener()])
-    loss_nan_hook = tf.train.NanTensorHook(
-      loss_tensor=tf.get_collection("loss"))
-    train_hooks = [step_count_hook, ckpt_saver_hook, loss_nan_hook, ]
-    return train_hooks
+  def train(self):
+    train_hooks = self._build_train_hooks()
+    self.estimator.train(
+      input_fn=functools.partial(self.model.input_fn,
+                                 self.hparams,
+                                 tf.estimator.ModeKeys.TRAIN),
+      hooks=train_hooks,
+      max_steps=self.hparams.train_steps)
 
-  def _create_eval_hooks(self):
-    save_eval_predictions_hook = SaveEvaluationPredictionsHook(
-      out_dir=self.hparams.out_dir)
-    return [save_eval_predictions_hook]
+  def eval(self):
+    eval_hooks = self._build_eval_hooks()
+    self.estimator.evaluate(
+      input_fn=functools.partial(self.model.input_fn,
+                                 self.hparams,
+                                 tf.estimator.ModeKeys.EVAL),
+      hooks=eval_hooks)
 
-  def _create_predict_hooks(self):
-    return None
+  def train_and_eval(self):
+    train_hooks = self._build_train_hooks()
+    eval_hooks = self._build_eval_hooks()
+    train_spec = tf.estimator.TrainSpec(
+      input_fn=functools.partial(self.model.input_fn,
+                                 self.hparams,
+                                 tf.estimator.ModeKeys.TRAIN),
+      hooks=train_hooks,
+      max_steps=self.hparams.train_steps)
+    # TODO(luozhouyang) Add exporters
+    eval_spec = tf.estimator.EvalSpec(
+      input_fn=functools.partial(self.model.input_fn,
+                                 self.hparams,
+                                 tf.estimator.ModeKeys.EVAL),
+      hooks=eval_hooks)
+    tf.estimator.train_and_evaluate(
+      self.estimator,
+      train_spec=train_spec,
+      eval_spec=eval_spec)
+
+  def predict(self):
+    predict_hooks = self._build_predict_hooks()
+    predictions = self.estimator.predict(
+      input_fn=functools.partial(self.model.input_fn,
+                                 self.hparams,
+                                 tf.estimator.ModeKeys.PREDICT),
+      hooks=predict_hooks)
+    # TODO(luozhouyang) save prediction results
+    results = text_utils.get_predictions(predictions,
+                                         self.hparams.eos,
+                                         self.hparams.subword_option)
+    print(results)
+
+  def export(self):
+    # TODO(luozhouyang) Add export ckpt path in hparams
+    self.estimator.export_savedmodel(
+      export_dir_base=os.path.join(self.hparams.out_dir, "export"),
+      serving_input_receiver_fn=self.model.serving_input_receiver_fn,
+      checkpoint_path=None)
+
+  def _build_train_hooks(self):
+    hooks = [InitHook(), CountParamsHook()]
+    return hooks
+
+  def _build_eval_hooks(self):
+    hooks = [InitHook()]
+    save_path = os.path.join(self.estimator.model_dir, "eval")
+    if not os.path.exists(save_path):
+      os.makedirs(save_path)
+    output_file = os.path.join(save_path, "predictions.txt")
+    hooks.append(SaveEvaluationPredictionsHook(
+      output_file=output_file,
+      eos=self.hparams.eos,
+      subword_option=self.hparams.subword_option))
+    return hooks
+
+  def _build_predict_hooks(self):
+    hooks = [InitHook()]
+    return hooks
 
 
-def main(unused_args):
-  hparams = Hparams(FLAGS).build()
-  naive_nmt = NaiveNMT(hparams)
-  if FLAGS.mode == "train":
-    naive_nmt.train()
-  elif FLAGS.mode == "eval":
-    naive_nmt.eval()
-  elif FLAGS.mode == "predict":
-    naive_nmt.predict()
-  elif FLAGS.mode == "export":
-    naive_nmt.export()
+def create_model(m, params):
+  if m == "basic_model":
+    return BasicModel(params=params)
+  elif m == "attention_model":
+    return AttentionModel(params=params)
+  elif m == "gnmt_model":
+    return GNMTModel(params=params)
   else:
-    raise ValueError("Unknown mode: %s" % FLAGS.mode)
+    raise ValueError("Invalid model type %s" % m)
 
 
-FLAGS = None
 if __name__ == "__main__":
+  tf.logging.set_verbosity(tf.logging.INFO)
+
   parser = argparse.ArgumentParser()
   parser.add_argument("--mode", type=str,
-                      choices=["train", "eval", "predict", "export"],
+                      choices=["train", "eval", "train_and_eval", "predict",
+                               "export"],
                       default="train",
                       help="Run mode.")
-  add_arguments(parser)
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  parser.add_argument("--model", type=str,
+                      choices=["basic_model", "attention_model", "gnmt_model"],
+                      default="basic_model",
+                      help="The model you want to use.")
+  parser.add_argument("--params_file", type=str,
+                      required=True,
+                      help="Params config file in JSON format.")
+  args, _ = parser.parse_known_args()
+  mode = args.mode
+  with open(args.params_file, mode="rt", encoding="utf8") as f:
+    configs = json.loads(f)
+  hparams = HParamsBuilder(dict_config=configs).build()
+  model = create_model(args.model, hparams)
+  naivenmt = NaiveNMT(hparams=hparams, model=model)
+  if mode == "train":
+    naivenmt.train()
+  elif mode == "eval":
+    naivenmt.eval()
+  elif mode == "train_and_eval":
+    naivenmt.train_and_eval()
+  elif mode == "predict":
+    naivenmt.predict()
+  elif model == "export":
+    naivenmt.export()
+  else:
+    raise ValueError("Invalid mode %s" % mode)
