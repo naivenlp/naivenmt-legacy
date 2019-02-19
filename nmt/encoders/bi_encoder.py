065330760843b764f45ba500a5bcb7d665c04ea9
luozhouyang <stupidme.me.lzy@gmail.com>
2019-02-19 19:13:55 +0800

    Add variable scope

--- nmt/encoders/bi_encoder.py
+++ nmt/encoders/bi_encoder.py
@@ -18,33 +18,35 @@ class BiRNNEncoder(EmbeddingEncoder):
 
         num_bi_layers = int(params['num_encoder_layers'] // 2)
         num_bi_residual_layers = int(params['num_encoder_residual_layers'] // 2)
-        # TODO(luozhouyang) Add residual_fn
-        fw_cell = rnn_utils.build_rnn_cells(
-            num_layers=num_bi_layers,
-            num_residual_layers=num_bi_residual_layers,
-            unit_type=params['unit_type'],
-            num_units=params['num_units'],
-            dropout=params['dropout'] if mode == tf.estimator.ModeKeys.TRAIN else 0.0,
-            forget_bias=params['forget_bias'],
-            residual_fn=None)
-        bw_cell = rnn_utils.build_rnn_cells(
-            num_layers=num_bi_layers,
-            num_residual_layers=num_bi_residual_layers,
-            unit_type=params['unit_type'],
-            num_units=params['num_units'],
-            dropout=params['dropout'],
-            forget_bias=params['forget_bias'],
-            residual_fn=None)
 
-        bi_outputs, bi_states = tf.nn.bidirectional_dynamic_rnn(
-            cell_fw=fw_cell,
-            cell_bw=bw_cell,
-            inputs=inputs,
-            dtype=self.dtype,
-            sequence_length=length,
-            time_major=params['time_major'],
-            swap_memory=params['swap_memory'])
-        outputs = tf.concat(bi_outputs, axis=-1)
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE) as scope:
+            fw_cell = rnn_utils.build_rnn_cells(
+                num_layers=num_bi_layers,
+                num_residual_layers=num_bi_residual_layers,
+                unit_type=params['unit_type'],
+                num_units=params['num_units'],
+                dropout=params['dropout'] if mode == tf.estimator.ModeKeys.TRAIN else 0.0,
+                forget_bias=params['forget_bias'],
+                residual_fn=None)
+            bw_cell = rnn_utils.build_rnn_cells(
+                num_layers=num_bi_layers,
+                num_residual_layers=num_bi_residual_layers,
+                unit_type=params['unit_type'],
+                num_units=params['num_units'],
+                dropout=params['dropout'],
+                forget_bias=params['forget_bias'],
+                residual_fn=None)
+
+            bi_outputs, bi_states = tf.nn.bidirectional_dynamic_rnn(
+                cell_fw=fw_cell,
+                cell_bw=bw_cell,
+                inputs=inputs,
+                dtype=self.dtype,
+                sequence_length=length,
+                time_major=params['time_major'],
+                swap_memory=params['swap_memory'])
+            outputs = tf.concat(bi_outputs, axis=-1)
+
         if params['time_major']:
             outputs = tf.transpose(outputs, perm=[1, 0, 2])
         # flatten states
--- nmt/encoders/gnmt_encoder.py
+++ nmt/encoders/gnmt_encoder.py
@@ -1,6 +1,7 @@
-from nmt.encoders.abstract_encoder import EmbeddingEncoder
 import tensorflow as tf
+
 from nmt import rnn_utils
+from nmt.encoders.abstract_encoder import EmbeddingEncoder
 
 
 class GNMTEncoder(EmbeddingEncoder):
@@ -17,47 +18,48 @@ class GNMTEncoder(EmbeddingEncoder):
         if params['time_major']:
             inputs = tf.transpose(inputs, perm=[1, 0, 2])
 
-        fw_cell = rnn_utils.build_rnn_cells(
-            num_layers=params['num_bi_layers'],
-            num_residual_layers=0,
-            unit_type=params['unit_type'],
-            num_units=params['num_units'],
-            dropout=params['dropout'] if mode == tf.estimator.ModeKeys.TRAIN else 0.0,
-            forget_bias=params['forget_bias'],
-            residual_fn=None)
-        bw_cell = rnn_utils.build_rnn_cells(
-            num_layers=params['num_bi_layers'],
-            num_residual_layers=0,
-            unit_type=params['unit_type'],
-            num_units=params['num_units'],
-            dropout=params['dropout'] if mode == tf.estimator.ModeKeys.TRAIN else 0.0,
-            forget_bias=params['forget_bias'],
-            residual_fn=None)
-        bi_outputs, bi_states = tf.nn.bidirectional_dynamic_rnn(
-            cell_fw=fw_cell,
-            cell_bw=bw_cell,
-            inputs=inputs,
-            sequence_length=length,
-            dtype=self.dtype,
-            time_major=params['time_major'],
-            swap_memory=params['swap_memory'])
-        states_bw = bi_states[1]
+        with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE) as scope:
+            fw_cell = rnn_utils.build_rnn_cells(
+                num_layers=params['num_bi_layers'],
+                num_residual_layers=0,
+                unit_type=params['unit_type'],
+                num_units=params['num_units'],
+                dropout=params['dropout'] if mode == tf.estimator.ModeKeys.TRAIN else 0.0,
+                forget_bias=params['forget_bias'],
+                residual_fn=None)
+            bw_cell = rnn_utils.build_rnn_cells(
+                num_layers=params['num_bi_layers'],
+                num_residual_layers=0,
+                unit_type=params['unit_type'],
+                num_units=params['num_units'],
+                dropout=params['dropout'] if mode == tf.estimator.ModeKeys.TRAIN else 0.0,
+                forget_bias=params['forget_bias'],
+                residual_fn=None)
+            bi_outputs, bi_states = tf.nn.bidirectional_dynamic_rnn(
+                cell_fw=fw_cell,
+                cell_bw=bw_cell,
+                inputs=inputs,
+                sequence_length=length,
+                dtype=self.dtype,
+                time_major=params['time_major'],
+                swap_memory=params['swap_memory'])
+            states_bw = bi_states[1]
 
-        uni_cell = rnn_utils.build_rnn_cells(
-            num_layers=params['num_encoder_layers'],
-            num_residual_layers=params['num_encoder_residual_layers'],
-            unit_type=params['unit_type'],
-            num_units=params['num_units'],
-            dropout=params['dropout'] if mode == tf.estimator.ModeKeys.TRAIN else 0.0,
-            forget_bias=params['forget_bias'],
-            residual_fn=None)
-        uni_outputs, uni_states = tf.nn.dynamic_rnn(
-            cell=uni_cell,
-            inputs=bi_outputs,
-            sequence_length=length,
-            dtype=self.dtype,
-            time_major=params['time_major'],
-            swap_memory=params['swap_memory'])
+            uni_cell = rnn_utils.build_rnn_cells(
+                num_layers=params['num_encoder_layers'],
+                num_residual_layers=params['num_encoder_residual_layers'],
+                unit_type=params['unit_type'],
+                num_units=params['num_units'],
+                dropout=params['dropout'] if mode == tf.estimator.ModeKeys.TRAIN else 0.0,
+                forget_bias=params['forget_bias'],
+                residual_fn=None)
+            uni_outputs, uni_states = tf.nn.dynamic_rnn(
+                cell=uni_cell,
+                inputs=bi_outputs,
+                sequence_length=length,
+                dtype=self.dtype,
+                time_major=params['time_major'],
+                swap_memory=params['swap_memory'])
 
         outputs, states = uni_outputs, uni_states
         if params['time_major']:
